50da833527437c9b320368946de000abacc7793d
Raul <rulonunhez@gmail.com>
2022-09-20 12:47:42 +0200

    Imagen en TableView

--- src/main/java/com/example/demofx/HelloApplication.java
+++ src/main/java/com/example/demofx/HelloApplication.java
@@ -17,76 +17,48 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
-import java.util.ArrayList;
 
 public class HelloApplication extends Application {
 
     private final Desktop desktop = Desktop.getDesktop();
-    private ArrayList<File> ficheros;
     private static final double WIDTH = 800;
     private static final double HEIGHT = 600;
+
+    private static final double IMG_WIDTH = 100;
+    private static final double IMG_HEIGHT = 100;
 
     ObservableList<TableItem> items;
 
     private TableView<TableItem> table;
 
-    public static class TableItem extends File{
+    /* Imagenes */
+    TableItem excel = new TableItem(new ImageView(new Image(new FileInputStream("C:\\Users\\HP\\IdeaProjects\\demoFX\\src\\main\\java\\com\\example\\demofx\\iconoExcel.png"))));
+    TableItem txt = new TableItem(new ImageView(new Image(new FileInputStream("C:\\Users\\HP\\IdeaProjects\\demoFX\\src\\main\\java\\com\\example\\demofx\\iconoTexto.jpg"))));
+    TableItem noExtension = new TableItem(new ImageView(new Image(new FileInputStream("C:\\Users\\HP\\IdeaProjects\\demoFX\\src\\main\\java\\com\\example\\demofx\\iconoSinExtension.png"))));
 
-        public Image imagen;
-
-        public TableItem(String pathname) {
-            super(pathname);
-        }
-
-        public TableItem(String parent, String child) {
-            super(parent, child);
-        }
-
-        public TableItem(File parent, String child) {
-            super(parent, child);
-        }
-
-        public TableItem(URI uri) {
-            super(uri);
-        }
-
-        public Image getImagen() {
-
-            try {
-                String fileType = Files.probeContentType(this.toPath());
-                switch (fileType) {
-                    case "text/plain" -> {
-                        return new Image("demofx/iconoTexto.jpg");
-                    }
-                    case "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" -> {
-                        return new Image("demofx/iconoExcel.png");
-                    }
-                    default -> {
-                        return new Image("demofx/iconoSinExtension.png");
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public void setImagen(Image imagen) {
-            this.imagen = imagen;
-        }
+    public HelloApplication() throws FileNotFoundException {
     }
 
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(new Group(), WIDTH, HEIGHT);
         Group root = (Group) scene.getRoot();
-        ficheros = new ArrayList<>();
         items = FXCollections.observableArrayList();
+
+        /* Detalle imagenes */
+        excel.getImagen().setFitWidth(IMG_WIDTH);
+        excel.getImagen().setFitHeight(IMG_HEIGHT);
+        txt.getImagen().setFitWidth(IMG_WIDTH);
+        txt.getImagen().setFitHeight(IMG_HEIGHT);
+        noExtension.getImagen().setFitWidth(IMG_WIDTH);
+        noExtension.getImagen().setFitHeight(IMG_HEIGHT);
 
         /* Barra de herramientas */
         ToolBar toolBar = new ToolBar();
@@ -95,24 +67,20 @@ public class HelloApplication extends Application {
         toolBar.getItems().add(new Separator());
 
         /* Button Events */
-        bArchivo.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                /* File chooser */
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Escoja un archivo para importar.");
-                File file = fileChooser.showOpenDialog(stage);
-                if (file != null) {
-                    ficheros.add(file);
-                    resetTableView();
-                }
+        bArchivo.setOnAction(actionEvent -> {
+            /* File chooser */
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Escoja un archivo para importar.");
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                resetTableView(file);
             }
         });
 
         /* Table View con ficheros */
         table = new TableView<>();
 
-        TableColumn<TableItem, Image> column1 = new TableColumn<>("FILES");
+        TableColumn<TableItem, ImageView> column1 = new TableColumn<>("FILES");
         column1.setPrefWidth(WIDTH);
         column1.setCellValueFactory(new PropertyValueFactory<>("imagen"));
 
@@ -121,12 +89,6 @@ public class HelloApplication extends Application {
         /* Scroll Bar */
         ScrollBar sb = new ScrollBar();
         sb.setOrientation(Orientation.VERTICAL);
-
-        /* Imagen */
-        ObservableList<Node> seq = root.getChildren();
-        Image imagen = new Image("com/example/demofx/iconoTexto.jpg");
-        addImageToObservableList(seq, 160, 20, 420, 120, imagen,
-                createImageCursor(imagen.getUrl(), 16, 16));
 
         VBox vbox = new VBox(toolBar, table);
         root.getChildren().add(sb);
@@ -140,33 +102,39 @@ public class HelloApplication extends Application {
         launch();
     }
 
-    public void resetTableView() {
-        items.add(new TableItem(ficheros.get(ficheros.size()-1).getPath()));
+    public void resetTableView(File file) {
+
+        try {
+            String fileType = Files.probeContentType(file.toPath());
+
+            switch (fileType) {
+                case "text/plain" -> items.add(txt);
+                case "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ->
+                        items.add(excel);
+                default -> items.add(noExtension);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         table.setItems(items);
     }
 
-    private static void addImageToObservableList(ObservableList<Node> seq,
-                                                 int x, int y,
-                                                 int w, int h,
-                                                 Image image,
-                                                 Cursor cursor) {
-        ImageView imageView = new ImageView();
-        imageView.setX(x);
-        imageView.setY(y);
-        imageView.setFitWidth(w);
-        imageView.setFitHeight(h);
-        imageView.setPreserveRatio(true);
-        imageView.setImage(image);
-        imageView.setCursor(cursor);
-        seq.add(imageView);
-    }
+    public static class TableItem{
 
-    private static Cursor createImageCursor(final String url,
-                                            final float hotspotX,
-                                            final float hotspotY) {
+        private ImageView imagen;
 
-        final Image cursorImage = new Image(url, 32, 32, false, true, true);
-        return new ImageCursor(cursorImage, hotspotX, hotspotY);
+        public TableItem(ImageView img) {
+            this.imagen = img;
+        }
+
+        public ImageView getImagen() {
+            return imagen;
+        }
+
+        public void setImagen(ImageView imagen) {
+            this.imagen = imagen;
+        }
     }
 
 
